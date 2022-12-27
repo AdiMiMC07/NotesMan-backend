fdf4450e0ef30f3c3f49395c701fd476e9740099
Aditya Mishra <adityavishu03@gmail.com>
2022-12-27 21:05:54 +0530

    fixed some bugs

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,11 @@ const { body, validationResult } = require('express-validator');
 const User = require('../model/User');
 const bcryptjs = require('bcryptjs');
 const jwToken = require('jsonwebtoken');
+require('dotenv').config();
 // const jwt_secret = "include<iostream>";
 const fetchuser = require('../middleware/fetchuser');
 const jwt_secret = process.env.JWT_SECRET;
-
+console.log(process.env.JWT_SECRET)
 // Route 1 : Creating create user endpoint :POST /api/auth/createuser : no login req
 router.post('/createuser', [body('name').isLength({ min: 1, max: 50 }), body('email').isEmail()], async (req, res) => {
   try {
